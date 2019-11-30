91f3dadab47fb80a74c29d6ab71c4b5685e1c095
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2019-11-30 17:59:13 +0800

    bug fix

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -33,15 +33,11 @@ class CardsController extends AppController
      * Deal cards to n players
      *
      * @return \Cake\Http\Response|null
-     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
-     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
-     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
      */
-    public function deal()
-    {
-        $number = $this->request->getQuery('number');
-        if(!is_numeric($number)){
-            $this->response->statusCode(403);
+    public function deal(){
+        $number_of_players = $this->request->getQuery('number');
+        if(!is_numeric($number_of_players) || (int)$number_of_players < 0){
+            $this->response->statusCode(400);
             $this->set([
                 'my_response' => ['error'=>'Input value does not exist or value is invalid'],
                 '_serialize' => 'my_response',
@@ -50,7 +46,33 @@ class CardsController extends AppController
             return;
         }
 
-        
+        $data = [
+            ['A', 'D'], ['2', 'D'], ['3', 'D'], ['4', 'D'], ['5', 'D'], ['6', 'D'], ['7', 'D'], ['8', 'D'], ['9', 'D'], ['X', 'D'], ['J', 'D'], ['Q', 'D'], ['K', 'D'],
+            ['A', 'H'], ['2', 'H'], ['3', 'H'], ['4', 'H'], ['5', 'H'], ['6', 'H'], ['7', 'H'], ['8', 'H'], ['9', 'H'], ['X', 'H'], ['J', 'H'], ['Q', 'H'], ['K', 'H'],
+            ['A', 'S'], ['2', 'S'], ['3', 'S'], ['4', 'S'], ['5', 'S'], ['6', 'S'], ['7', 'S'], ['8', 'S'], ['9', 'S'], ['X', 'S'], ['J', 'S'], ['Q', 'S'], ['K', 'S'],
+            ['A', 'C'], ['2', 'C'], ['3', 'C'], ['4', 'C'], ['5', 'C'], ['6', 'C'], ['7', 'C'], ['8', 'C'], ['9', 'C'], ['X', 'C'], ['J', 'C'], ['Q', 'C'], ['K', 'C'],
+        ];
+
+        shuffle($data);
+        $number_of_players = (int)$number_of_players;
+        $response = [];
+
+        if($number_of_players > 0){
+            $chunk_size = (int)(count($data) / $number_of_players);
+
+            if($chunk_size == 0){
+                $chunk_size = 1;
+            }
+
+            $response = array_chunk($data, $chunk_size);
+            $response = array_pad($response, $number_of_players, []);
+        }
+
+        $this->set([
+            'my_response' => $response,
+            '_serialize' => 'my_response',
+        ]);
+
         $this->RequestHandler->renderAs($this, 'json');
     }
 }
