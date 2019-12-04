17a252ecbc1d9f675c42fd2a60c1620e85838a2e
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2019-12-04 19:18:45 +0800

    add more tests

--- tests/TestCase/Controller/CardsControllerTest.php
+++ tests/TestCase/Controller/CardsControllerTest.php
@@ -13,9 +13,29 @@ class CardsControllerTest extends IntegrationTestCase
 {
     public function testPositiveInteger()
     {
-        $result = $this->get('/deal?number=9');
+        $this->get('/deal?number=9');
         $this->assertResponseOk();
-        echo $this->_response->getBody();
         $this->assertEquals(9, count(json_decode($this->_response->getBody())));
+    }
+
+    public function testNegativeInteger()
+    {
+        $this->get('/deal?number=-3');
+        $this->assertResponseCode(400);
+        $this->assertEquals('Input value does not exist or value is invalid', json_decode($this->_response->getBody())->error);
+    }
+
+    public function testNoParameter()
+    {
+        $this->get('/deal');
+        $this->assertResponseCode(400);
+        $this->assertEquals('Input value does not exist or value is invalid', json_decode($this->_response->getBody())->error);
+    }
+
+    public function testMoreThan53()
+    {
+        $this->get('/deal?number=60');
+        $this->assertResponseOk();
+        $this->assertEquals(60, count(json_decode($this->_response->getBody())));
     }
 }
